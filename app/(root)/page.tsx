8f5f8235691dbda76b16c2700486b497957ce1f2
Chandu Gangola <chandugangola444@gmail.com>
2025-01-21 12:08:20 +0530

    some minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,10 +4,9 @@ import { getLatestProducts } from "@/lib/actions/product.action";
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   return ( 
-  <>
+  <div className='space-y-8'>
     <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
-    
-  </>
+  </div>
   );
 }
  
